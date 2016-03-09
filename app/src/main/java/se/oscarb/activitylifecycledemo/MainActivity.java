c80f5f559e9d1dd9f28d67395801755389a8c90b
idrougge <idrougge@mac.com>
2016-03-09 13:37:59 +0100

    Lite beskrivningar av on*-metoderna

--- app/src/main/java/se/oscarb/activitylifecycledemo/MainActivity.java
+++ app/src/main/java/se/oscarb/activitylifecycledemo/MainActivity.java
@@ -24,32 +24,38 @@ public class MainActivity extends AppCompatActivity {
         Log.i("Activity Lifecycle", "Metoden onCreate() körs!");
     }
     // Metoden körs när appen förstörs // Henry
+    // Körs när en aktivitet tas bort för att spara minne, t ex vid övergång till/från en annan aktivitet. / Iggy
     @Override
     protected void onDestroy() {
         super.onDestroy();
     }
     // Metoden körs när appen pausar // Henry
+    // Körs när man går in i en annan aktivitet eller visar dialoger / Iggy
     @Override
     protected void onPause() {
         super.onPause();
     }
     // Metoden körs när appen startar om // Henry
+    // Körs när aktiviteten återupptas efter onStop() / Iggy
     @Override
     protected void onRestart() {
         super.onRestart();
         
     }
     // Metoden körs när appen körs igen // Henry
+    // Körs när man går in i aktiviteten igen efter att den tidigare pausats med onPause() / Iggy
     @Override
     protected void onResume() {
         super.onResume();
     }
     // Metoden körs när appen startar om // Henry
+    // Körs när aktiviteten startas efter att tidigare ha skapats med onCreate() eller stoppats med onStop()
     @Override
     protected void onStart() {
         super.onStart();
     }
     // Metoden körs när appen slutar // Henry
+    // Körs när aktiviteten ska läggas ner inför övergång till annan aktivitet eller man lämnar appen
     @Override
     protected void onStop() {
         super.onStop();
